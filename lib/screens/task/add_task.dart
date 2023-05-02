3f7911877c68aabe52d1fca3941225647f440f3b
Kola132 <kolmakovn111@gmail.com>
2023-05-02 23:31:52 +0300

    обновил DropdownMenu

--- lib/screens/task/add_task.dart
+++ lib/screens/task/add_task.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 
 import '../../models/task.dart';
 
-
 class AddTask extends StatefulWidget {
   TextEditingController controller = TextEditingController();
-  List<Task> tasks;
-  AddTask(this.tasks);
+  List<Task> tasksList;
+  AddTask(this.tasksList);
 
   @override
   State<AddTask> createState() => _AddTaskState();
 }
 
 class _AddTaskState extends State<AddTask> {
-  TimeOfDay _time = TimeOfDay(hour: 0, minute: 0);
+  TimeOfDay _time = const TimeOfDay(hour: 0, minute: 0);
   String data = 'data';
   List<String> sections = ['Personal', 'Work', 'Health'];
 
@@ -35,19 +34,15 @@ class _AddTaskState extends State<AddTask> {
     // final Task newTask = Task();
     final List<DropdownMenuEntry<Chapsters>> tasks =
         <DropdownMenuEntry<Chapsters>>[];
-    final List<DropdownMenuEntry<Chapsters>> colorEntries =
-        <DropdownMenuEntry<Chapsters>>[];
-    // for (final Task color in widget.tasks) {
-    //   if (color.title != null) {
-    //     Chapsters chapsters = Chapsters(color.title!);
-    //     tasks.add(DropdownMenuEntry<Chapsters>(
-    //         value: chapsters, label: color.title!));
-    //   }
-    // }
-    // for (final String color in Sections) {
-    //   colorEntries.add(DropdownMenuEntry<Sections>(
-    //       value: color, label: color.));
-    // }
+    for (final Task color in widget.tasksList) {
+      if (color.title != null) {
+        tasks.add(DropdownMenuEntry<Chapsters>(
+          value: Chapsters(color.title!),
+          label: color.title!,
+        ));
+      }
+    }
+
 
     return Scaffold(
         body: SafeArea(
@@ -55,14 +50,14 @@ class _AddTaskState extends State<AddTask> {
         child: ListView(
           children: [
             Container(
-              margin: EdgeInsets.all(8),
+              margin: const EdgeInsets.all(8),
               height: 140,
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 85, 111, 239),
+                color: const Color.fromARGB(255, 85, 111, 239),
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
-                    color: Color.fromARGB(255, 7, 15, 255).withOpacity(0.5),
+                    color: const Color.fromARGB(255, 7, 15, 255).withOpacity(0.5),
                     spreadRadius: 2,
                     blurRadius: 12,
                     offset: const Offset(3, 3),
@@ -75,15 +70,17 @@ class _AddTaskState extends State<AddTask> {
                 children: [
                   Text(
                     '$data',
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Color.fromARGB(255, 27, 27, 27),
                       fontSize: 26,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   const SizedBox(height: 20),
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [Text(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
                         '$_time',
                         style: const TextStyle(
                           color: Colors.white60,
@@ -113,9 +110,7 @@ class _AddTaskState extends State<AddTask> {
                       TextField(
                         controller: widget.controller,
                         decoration: const InputDecoration(
-                          
                           labelText: 'Task name',
-
                         ),
                         onSubmitted: (String name) {
                           setState(() {
@@ -124,29 +119,29 @@ class _AddTaskState extends State<AddTask> {
                           });
                         },
                       ),
-                      Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
                           ElevatedButton(
                             onPressed: () {
                               _selectTime();
                               newDesk['time'] = _time.toString();
-                              
                             },
                             child: const Text('Время'),
                           ),
                           DropdownMenu<Chapsters>(
-                            width:  150,
-                            dropdownMenuEntries: colorEntries,
+                            width: 150,
+                            dropdownMenuEntries: tasks,
                             enableSearch: false,
                             enableFilter: false,
-                            label: Text("Раздел"),
+                            label: const Text("Раздел"),
                           ),
-                          
                         ],
-                      ),ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Готовонах'),
-                          ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Готовонах'),
+                      ),
                     ],
                   )),
             ),
@@ -157,32 +152,7 @@ class _AddTaskState extends State<AddTask> {
   }
 }
 
-enum Sections {
-  personal('Personal'),
-  work('Work'),
-  health('Health');
-
-  const Sections(
-    this.label,
-  );
-  final String label;
-// int index;
-}
-
 class Chapsters {
   final String label;
-
   Chapsters(this.label);
-}
-
-enum ColorLabel {
-  blue('Blue', Colors.red),
-  pink('Pink', Colors.pink),
-  green('Green', Colors.green),
-  yellow('Yellow', Colors.yellow),
-  grey('Grey', Colors.grey);
-
-  const ColorLabel(this.label, this.color);
-  final String label;
-  final Color color;
 }
